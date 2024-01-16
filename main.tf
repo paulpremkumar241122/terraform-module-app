c401f9b24dcb7b3f2e71b3490da66e864282f3cc
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-16 06:04:27 +0530

    merge fuction

--- main.tf
+++ main.tf
@@ -86,10 +86,10 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
-  tags = {
+  tags = merge({
     Name = "${var.component}-${var.env}"
-  }
-
+    },
+    var.tags)
 }
 
 #### DNS Rout_53
