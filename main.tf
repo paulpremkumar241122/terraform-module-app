9fe49027b41f292e820143bb5b00670f7afc595f
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-23 03:40:24 +0530

    output "subnet_ids"

--- main.tf
+++ main.tf
@@ -1,58 +1,129 @@
-## Policy
+### Policy
+#
+#resource "aws_iam_policy" "policy" {
+#  name        = "${var.component}-${var.env}-ssm-pm-policy"
+#  path        = "/"
+#  description = "${var.component}-${var.env}-ssm-pm-policy"
+#
+#  policy = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "VisualEditor0",
+#        "Effect": "Allow",
+#        "Action": [
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameters",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource": "arn:aws:ssm:us-east-1:461355683695:parameter/roboshop.${var.env}.${var.component}.*"
+#      }
+#    ]
+#  })
+#}
+#
+##### IAM Role
+#
+#resource "aws_iam_role" "role" {
+#  name = "${var.component}-${var.env}-ec2-role"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole"
+#        Effect = "Allow"
+#        Sid    = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#}
+#
+#resource "aws_iam_instance_profile" "instance_profile" {
+#  name = "${var.component}-${var.env}-ec2-role"
+#  role = aws_iam_role.role.name
+#}
+#
+#resource "aws_iam_role_policy_attachment" "policy-attach" {
+#  role       = aws_iam_role.role.name
+#  policy_arn = aws_iam_policy.policy.arn
+#}
+#
+###### Security Group
+#
+#resource "aws_security_group" "sg" {
+#  name        = "${var.component}-${var.env}-sg"
+#  description = "${var.component}-${var.env}-sg"
+#
+#  ingress {
+#    description      = "SSH"
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  tags = {
+#    Name = "${var.component}-${var.env}-sg"
+#  }
+#}
+#
+##### EC2 instance
+#
+#resource "aws_instance" "instance" {
+#  ami                    = data.aws_ami.ami.id
+#  instance_type          = "t3.small"
+#  vpc_security_group_ids = [aws_security_group.sg.id]
+#  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+#
+#  tags = merge ({
+#    Name = "${var.component}-${var.env}"
+#    },
+#    var.tags)
+#}
+#
+##### DNS Rout_53
+#
+#resource "aws_route53_record" "dns" {
+#  zone_id = "Z0866621F4YFMPDO5E0L"
+#  name    = "${var.component}-dev"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance.private_ip]
+#}
+#
+###### null Resource
+#
+#resource "null_resource" "ansible" {
+#  depends_on = [aws_instance.instance,aws_route53_record.dns]
+#  provisioner "remote-exec" {
+#
+#    connection {
+#      type     = "ssh"
+#      user     = "centos"
+#      password = "DevOps321"
+#      host     = aws_instance.instance.public_ip
+#    }
+#    inline = [
+#      "sudo labauto ansible",
+#      "sudo set-hostname -skip-apply ${var.component}",
+#      "ansible-pull -i localhost, -U https://github.com/paulpremkumar241122/roboshop-ansible.git main.yml -e env=${var.env} -e role_name=${var.component}"
+#    ]
+#  }
+#}
 
-resource "aws_iam_policy" "policy" {
-  name        = "${var.component}-${var.env}-ssm-pm-policy"
-  path        = "/"
-  description = "${var.component}-${var.env}-ssm-pm-policy"
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:461355683695:parameter/roboshop.${var.env}.${var.component}.*"
-      }
-    ]
-  })
-}
-
-#### IAM Role
-
-resource "aws_iam_role" "role" {
-  name = "${var.component}-${var.env}-ec2-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component}-${var.env}-ec2-role"
-  role = aws_iam_role.role.name
-}
-
-resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
-##### Security Group
+###### Security Group
 
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
@@ -78,46 +149,14 @@ resource "aws_security_group" "sg" {
   }
 }
 
-#### EC2 instance
-
-resource "aws_instance" "instance" {
-  ami                    = data.aws_ami.ami.id
-  instance_type          = "t3.small"
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  subnet_id = var.subnet_id
 
-  tags = merge ({
-    Name = "${var.component}-${var.env}"
-    },
-    var.tags)
-}
 
-#### DNS Rout_53
-
-resource "aws_route53_record" "dns" {
-  zone_id = "Z0866621F4YFMPDO5E0L"
-  name    = "${var.component}-dev"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance.private_ip]
-}
-
-##### null Resource
-
-resource "null_resource" "ansible" {
-  depends_on = [aws_instance.instance,aws_route53_record.dns]
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance.public_ip
-    }
-    inline = [
-      "sudo labauto ansible",
-      "sudo set-hostname -skip-apply ${var.component}",
-      "ansible-pull -i localhost, -U https://github.com/paulpremkumar241122/roboshop-ansible.git main.yml -e env=${var.env} -e role_name=${var.component}"
-    ]
+  tags = {
+    Name = var.component
   }
 }
