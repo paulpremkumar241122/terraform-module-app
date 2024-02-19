280e1fa028b43fdd56921c48241fc30c3f2b44a6
Paul Prem Kumar <paul.prem5@gmail.com>
2024-02-19 12:32:14 +0530

    kms arn and kms decrypt for docdb cluster and modified parameters.

--- iam.tf
+++ iam.tf
@@ -15,9 +15,14 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
-          "ssm:GetParameter"
+          "ssm:GetParameter",
+          "kms:Decrypt"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:461355683695:parameter/roboshop.${var.env}.${var.component}.*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:461355683695:parameter/roboshop.${var.env}.${var.component}.*",
+          "arn:aws:ssm:us-east-1:461355683695:parameter/roboshop.${var.env}.docdb.*",
+          var.kms_arn
+        ]
       }
     ]
   })
