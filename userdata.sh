b9dee11d7b8bc98579899d88f0e5d1a632b022f8
Paul Prem Kumar <paul.prem5@gmail.com>
2024-02-22 07:34:56 +0530

    sleep 120

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sleep 120
 sudo set-hostname -skip-apply ${component}
 labauto ansible
 ansible-pull -i localhost, -U https://github.com/paulpremkumar241122/roboshop-ansible.git main.yml -e role_name=${component} -e env=${env} &>>/opt/ansible.log
